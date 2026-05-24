e6a558ef0924134a799b5f075149e0dd6b360304
vaibhav-suruse <vaibhavsuruse@gmail.com>
2026-05-24 10:26:23 +0530

    Fix syntax error in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace WebApplicationSampleTest2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Error()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    "Logs/log-.txt",
+                    rollingInterval: RollingInterval.Day
+                )
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+                    webBuilder.UseUrls($"http://0.0.0.0:{port}");
+                    webBuilder.UseStartup << Startup > ();
+                });
+    }
+}
+
+
+
+
 //using Microsoft.AspNetCore.Hosting;
 //using Microsoft.Extensions.Configuration;
 //using Microsoft.Extensions.Hosting;
@@ -39,41 +79,3 @@
 
 
 
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using System;
-
-namespace WebApplicationSampleTest2
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Error()
-                .WriteTo.Console()
-                .WriteTo.File(
-                    "Logs/log-.txt",
-                    rollingInterval: RollingInterval.Day
-                )
-                .CreateLogger();
-
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    // THIS IS CRITICAL - Use Render's PORT
-                    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-                    webBuilder.UseUrls($"http://0.0.0.0:{port}");
-                    webBuilder.UseStartup << Startup > ();
-                });
-    }
-}
