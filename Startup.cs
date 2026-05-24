c082488a43c94e67d5d0b9a1bb37a8565e49fc13
vaibhav-suruse <vaibhavsuruse@gmail.com>
2026-05-24 14:45:36 +0530

    Fix Program.cs syntax

--- Startup.cs
+++ Startup.cs
@@ -86,9 +86,10 @@ namespace WebApplicationSampleTest2
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-               // app.UseHsts();
+                // app.UseHsts();
             }
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
