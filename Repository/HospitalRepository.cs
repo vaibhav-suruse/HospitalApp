1843f18881bf3fa1ff278b62509f06c8a449e3e4
vaibhav-suruse <vaibhavsuruse@gmail.com>
2026-05-25 15:19:35 +0530

    change sp name to uppercase

--- Repository/HospitalRepository.cs
+++ Repository/HospitalRepository.cs
@@ -228,7 +228,7 @@ namespace WebApplicationSampleTest2.Repository
                 using (var conn = new MySqlConnection(_connectionString))
                 {
                     conn.Open();
-                    using (var cmd = new MySqlCommand("sp_getmainhospitals", conn))
+                    using (var cmd = new MySqlCommand("sp_getMainhospitals", conn))
                     {
                         cmd.CommandType = CommandType.StoredProcedure;
 
